ecb61e681a82612ca422fe64b77790c62f32880b
guirdgrs <gjrodrigues.japa@hotmail.com>
2025-08-08 17:17:52 -0300

    feat: Aprendendo sobre templates

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 # Uma view function é uma função que recebe um pedido e retorna uma resposta ao cliente
-# É um manipulador de solicitações
+# É mais um manipulador de solicitações do que uma 'view' de fato
 
 # Essa função deve ter um request e retornar um response
 def say_hello(request):
     # Aqui é possível trazer dados do DB / Transformar esses dados / Mandar e-mail
-    return HttpResponse('Hello World!')
+    return render(request, 'hello.html', {'name': 'Kuro'})
+    # retorna um HttpResponse com o template renderizado e passa um name para o template
